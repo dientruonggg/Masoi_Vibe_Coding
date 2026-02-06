4395cd0f121f909f136a43a00f227c7b9d575d82
dientruonggg <dientruong2104@gmail.com>
2026-02-06 09:15:45 +0700

    fix: configure Socket.IO for Render.com (CORS + transports)

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    },
+    transports: ['websocket', 'polling'],
+    allowEIO3: true
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
